1f76600fa8d7923028d4257dd7b90092a7a30904
maha-hajja <maha.d.hajja@gmail.com>
2023-05-12 11:39:37 +0100

    setup

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/conduitio/conduit-connector-connectorname
+module github.com/conduitio-labs/conduit-connector-slack
 
 go 1.20
 
--- spec.go
+++ spec.go
@@ -1,4 +1,18 @@
-package connectorname
+// Copyright © 2023 Meroxa, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package slack
 
 import (
 	sdk "github.com/conduitio/conduit-connector-sdk"
@@ -11,7 +25,7 @@ var version = "(devel)"
 // Specification returns the connector's specification.
 func Specification() sdk.Specification {
 	return sdk.Specification{
-		Name:        "connectorname",
+		Name:        "slack",
 		Summary:     "<describe your connector>",
 		Description: "<describe your connector in detail>",
 		Version:     version,
